39d0b8003af14939bbb0d5152b3a79538e6c4cc7
val <valentin.crettaz@consulthys.com>
2016-06-09 13:16:47 +0200

    Clean up tabs

--- beater/logstashbeat.go
+++ beater/logstashbeat.go
@@ -2,124 +2,124 @@ package beater
 
 import (
     "errors"
-	"fmt"
-	"net/url"
-	"time"
+    "fmt"
+    "net/url"
+    "time"
 
-	"github.com/elastic/beats/libbeat/beat"
-	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/logp"
-	"github.com/elastic/beats/libbeat/publisher"
+    "github.com/elastic/beats/libbeat/beat"
+    "github.com/elastic/beats/libbeat/common"
+    "github.com/elastic/beats/libbeat/logp"
+    "github.com/elastic/beats/libbeat/publisher"
 
-	"github.com/consulthys/logstashbeat/config"
+    "github.com/consulthys/logstashbeat/config"
 )
 
 const selector = "logstashbeat"
 
 type Logstashbeat struct {
-	period       time.Duration
-	urls         []*url.URL
+    period       time.Duration
+    urls         []*url.URL
 
-	beatConfig   *config.Config
+    beatConfig   *config.Config
 
-	done         chan struct{}
-	client       publisher.Client
+    done         chan struct{}
+    client       publisher.Client
 
-	eventsStats  bool
-	jvmStats     bool
-	processStats bool
+    eventsStats  bool
+    jvmStats     bool
+    processStats bool
 }
 
 // Creates beater
 func New() *Logstashbeat {
-	return &Logstashbeat{
-		done: make(chan struct{}),
-	}
+    return &Logstashbeat{
+        done: make(chan struct{}),
+    }
 }
 
 /// *** Beater interface methods ***///
 
 func (bt *Logstashbeat) Config(b *beat.Beat) error {
 
-	// Load beater beatConfig
-	err := b.RawConfig.Unpack(&bt.beatConfig)
-	if err != nil {
-		return fmt.Errorf("Error reading config file: %v", err)
-	}
+    // Load beater beatConfig
+    err := b.RawConfig.Unpack(&bt.beatConfig)
+    if err != nil {
+        return fmt.Errorf("Error reading config file: %v", err)
+    }
 
-	//define default URL if none provided
-	var urlConfig []string
-	if bt.beatConfig.Logstashbeat.URLs != nil {
-		urlConfig = bt.beatConfig.Logstashbeat.URLs
-	} else {
-		urlConfig = []string{"http://127.0.0.1:9600"}
-	}
+    //define default URL if none provided
+    var urlConfig []string
+    if bt.beatConfig.Logstashbeat.URLs != nil {
+        urlConfig = bt.beatConfig.Logstashbeat.URLs
+    } else {
+        urlConfig = []string{"http://127.0.0.1:9600"}
+    }
 
-	bt.urls = make([]*url.URL, len(urlConfig))
-	for i := 0; i < len(urlConfig); i++ {
-		u, err := url.Parse(urlConfig[i])
-		if err != nil {
-			logp.Err("Invalid Logstash URL: %v", err)
-			return err
-		}
-		bt.urls[i] = u
-	}
+    bt.urls = make([]*url.URL, len(urlConfig))
+    for i := 0; i < len(urlConfig); i++ {
+        u, err := url.Parse(urlConfig[i])
+        if err != nil {
+            logp.Err("Invalid Logstash URL: %v", err)
+            return err
+        }
+        bt.urls[i] = u
+    }
 
-	if bt.beatConfig.Logstashbeat.Stats.Events != nil {
-		bt.eventsStats = *bt.beatConfig.Logstashbeat.Stats.Events
-	} else {
-		bt.eventsStats = true
-	}
+    if bt.beatConfig.Logstashbeat.Stats.Events != nil {
+        bt.eventsStats = *bt.beatConfig.Logstashbeat.Stats.Events
+    } else {
+        bt.eventsStats = true
+    }
 
-	if bt.beatConfig.Logstashbeat.Stats.JVM != nil {
-		bt.jvmStats = *bt.beatConfig.Logstashbeat.Stats.JVM
-	} else {
-		bt.jvmStats = true
-	}
+    if bt.beatConfig.Logstashbeat.Stats.JVM != nil {
+        bt.jvmStats = *bt.beatConfig.Logstashbeat.Stats.JVM
+    } else {
+        bt.jvmStats = true
+    }
 
-	if bt.beatConfig.Logstashbeat.Stats.Process != nil {
-		bt.processStats = *bt.beatConfig.Logstashbeat.Stats.Process
-	} else {
-		bt.processStats = true
-	}
+    if bt.beatConfig.Logstashbeat.Stats.Process != nil {
+        bt.processStats = *bt.beatConfig.Logstashbeat.Stats.Process
+    } else {
+        bt.processStats = true
+    }
 
-	if !bt.eventsStats && !bt.jvmStats && !bt.processStats {
-		return errors.New("Invalid statistics configuration")
-	}
+    if !bt.eventsStats && !bt.jvmStats && !bt.processStats {
+        return errors.New("Invalid statistics configuration")
+    }
 
-	return nil
+    return nil
 }
 
 func (bt *Logstashbeat) Setup(b *beat.Beat) error {
 
-	// Setting default period if not set
-	if bt.beatConfig.Logstashbeat.Period == "" {
-		bt.beatConfig.Logstashbeat.Period = "10s"
-	}
+    // Setting default period if not set
+    if bt.beatConfig.Logstashbeat.Period == "" {
+        bt.beatConfig.Logstashbeat.Period = "10s"
+    }
 
-	bt.client = b.Publisher.Connect()
+    bt.client = b.Publisher.Connect()
 
-	var err error
-	bt.period, err = time.ParseDuration(bt.beatConfig.Logstashbeat.Period)
-	if err != nil {
-		return err
-	}
+    var err error
+    bt.period, err = time.ParseDuration(bt.beatConfig.Logstashbeat.Period)
+    if err != nil {
+        return err
+    }
 
-	logp.Debug(selector, "Init logstashbeat")
-	logp.Debug(selector, "Period %v\n", bt.period)
-	logp.Debug(selector, "Watch %v", bt.urls)
-	logp.Debug(selector, "Events statistics %t\n", bt.eventsStats)
-	logp.Debug(selector, "JVM statistics %t\n", bt.jvmStats)
-	logp.Debug(selector, "Process statistics %t\n", bt.processStats)
+    logp.Debug(selector, "Init logstashbeat")
+    logp.Debug(selector, "Period %v\n", bt.period)
+    logp.Debug(selector, "Watch %v", bt.urls)
+    logp.Debug(selector, "Events statistics %t\n", bt.eventsStats)
+    logp.Debug(selector, "JVM statistics %t\n", bt.jvmStats)
+    logp.Debug(selector, "Process statistics %t\n", bt.processStats)
 
-	return nil
+    return nil
 }
 
 func (bt *Logstashbeat) Run(b *beat.Beat) error {
-	logp.Info("logstashbeat is running! Hit CTRL-C to stop it.")
+    logp.Info("logstashbeat is running! Hit CTRL-C to stop it.")
 
-	for _, u := range bt.urls {
-		go func(u *url.URL) {
+    for _, u := range bt.urls {
+        go func(u *url.URL) {
 
             ticker := time.NewTicker(bt.period)
             counter := 1
@@ -130,16 +130,16 @@ func (bt *Logstashbeat) Run(b *beat.Beat) error {
                 case <-ticker.C:
                 }
 
-				timerStart := time.Now()
+                timerStart := time.Now()
 
                 if bt.eventsStats {
-					logp.Debug(selector, "Events stats for url: %v", u)
-					events_stats, err := bt.GetEventsStats(*u)
+                    logp.Debug(selector, "Events stats for url: %v", u)
+                    events_stats, err := bt.GetEventsStats(*u)
 
-					if err != nil {
-						logp.Err("Error reading events stats: %v", err)
-					} else {
-						logp.Debug(selector, "Events stats detail: %+v", events_stats)
+                    if err != nil {
+                        logp.Err("Error reading events stats: %v", err)
+                    } else {
+                        logp.Debug(selector, "Events stats detail: %+v", events_stats)
 
                         event := common.MapStr{
                             "@timestamp": common.Time(time.Now()),
@@ -151,17 +151,17 @@ func (bt *Logstashbeat) Run(b *beat.Beat) error {
                         bt.client.PublishEvent(event)
                         logp.Info("Logstash events stats sent")
                         counter++
-					}
+                    }
                 }
 
                 if bt.jvmStats {
-					logp.Debug(selector, "JVM stats for url: %v", u)
-					jvm_stats, err := bt.GetJvmStats(*u)
+                    logp.Debug(selector, "JVM stats for url: %v", u)
+                    jvm_stats, err := bt.GetJvmStats(*u)
 
-					if err != nil {
-						logp.Err("Error reading JVM stats: %v", err)
-					} else {
-						logp.Debug(selector, "JVM stats detail: %+v", jvm_stats)
+                    if err != nil {
+                        logp.Err("Error reading JVM stats: %v", err)
+                    } else {
+                        logp.Debug(selector, "JVM stats detail: %+v", jvm_stats)
 
                         event := common.MapStr{
                             "@timestamp":   common.Time(time.Now()),
@@ -170,20 +170,20 @@ func (bt *Logstashbeat) Run(b *beat.Beat) error {
                             "jvm":          jvm_stats.Jvm,
                         }
 
-						bt.client.PublishEvent(event)
+                        bt.client.PublishEvent(event)
                         logp.Info("Logstash JVM stats sent")
                         counter++
-					}
+                    }
                 }
 
                 if bt.processStats {
-					logp.Debug(selector, "Process stats for url: %v", u)
-					process_stats, err := bt.GetProcessStats(*u)
+                    logp.Debug(selector, "Process stats for url: %v", u)
+                    process_stats, err := bt.GetProcessStats(*u)
 
-					if err != nil {
-						logp.Err("Error reading process stats: %v", err)
-					} else {
-						logp.Debug(selector, "Process stats detail: %+v", process_stats)
+                    if err != nil {
+                        logp.Err("Error reading process stats: %v", err)
+                    } else {
+                        logp.Debug(selector, "Process stats detail: %+v", process_stats)
 
                         event := common.MapStr{
                             "@timestamp": common.Time(time.Now()),
@@ -192,32 +192,32 @@ func (bt *Logstashbeat) Run(b *beat.Beat) error {
                             "process": process_stats.Process,
                         }
 
-						bt.client.PublishEvent(event)
+                        bt.client.PublishEvent(event)
                         logp.Info("Logstash process stats sent")
                         counter++
-					}
+                    }
                 }
 
-				timerEnd := time.Now()
-				duration := timerEnd.Sub(timerStart)
-				if duration.Nanoseconds() > bt.period.Nanoseconds() {
-					logp.Warn("Ignoring tick(s) due to processing taking longer than one period")
-				}
+                timerEnd := time.Now()
+                duration := timerEnd.Sub(timerStart)
+                if duration.Nanoseconds() > bt.period.Nanoseconds() {
+                    logp.Warn("Ignoring tick(s) due to processing taking longer than one period")
+                }
             }
 
-		GotoFinish:
-		}(u)
-	}
+        GotoFinish:
+        }(u)
+    }
 
-	<-bt.done
-	return nil
+    <-bt.done
+    return nil
 }
 
 func (bt *Logstashbeat) Cleanup(b *beat.Beat) error {
-	return nil
+    return nil
 }
 
 func (bt *Logstashbeat) Stop() {
-	logp.Debug(selector, "Stop logstashbeat")
-	close(bt.done)
+    logp.Debug(selector, "Stop logstashbeat")
+    close(bt.done)
 }
--- beater/nodes.go
+++ beater/nodes.go
@@ -1,12 +1,12 @@
 package beater
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "strings"
 )
 
 const NODE_EVENTS_STATS = "/_node/stats/events"
@@ -14,34 +14,34 @@ const NODE_JVM_STATS = "/_node/stats/jvm"
 const NODE_PROCESS_STATS = "/_node/stats/process"
 
 type EventsStats struct {
-	Events struct {
-		In uint64 `json:"in"`
-		Filtered uint64 `json:"filtered"`
-		Out uint64 `json:"out"`
-	}
+    Events struct {
+        In uint64 `json:"in"`
+        Filtered uint64 `json:"filtered"`
+        Out uint64 `json:"out"`
+    }
 }
 
 type JvmStats struct {
     Jvm struct {
-		Threads struct {
-			Count      uint64 `json:"count"`
-			Peak_count uint64 `json:"peak_count"`
-		} `json:"threads"`
-	}
+        Threads struct {
+            Count      uint64 `json:"count"`
+            Peak_count uint64 `json:"peak_count"`
+        } `json:"threads"`
+    }
 }
 type ProcessStats struct {
     Process struct {
-		Open_file_descriptors int64 `json:"open_file_descriptors"`
-		Peak_open_file_descriptors int64 `json:"peak_open_file_descriptors"`
-		Max_file_descriptors  int64 `json:"max_file_descriptors"`
-		Cpu struct {
-			Percent         uint64 `json:"percent"`
-			Total_in_millis uint64 `json:"total_in_millis"`
-		} `json:"cpu"`
-		Mem struct {
-			Total_virtual_in_bytes uint64 `json:"total_virtual_in_bytes"`
-		} `json:"mem"`
-	}
+        Open_file_descriptors int64 `json:"open_file_descriptors"`
+        Peak_open_file_descriptors int64 `json:"peak_open_file_descriptors"`
+        Max_file_descriptors  int64 `json:"max_file_descriptors"`
+        Cpu struct {
+            Percent         uint64 `json:"percent"`
+            Total_in_millis uint64 `json:"total_in_millis"`
+        } `json:"cpu"`
+        Mem struct {
+            Total_virtual_in_bytes uint64 `json:"total_virtual_in_bytes"`
+        } `json:"mem"`
+    }
 }
 
 func (bt *Logstashbeat) GetEventsStats(u url.URL) (*EventsStats, error) {
@@ -66,7 +66,7 @@ func (bt *Logstashbeat) GetEventsStats(u url.URL) (*EventsStats, error) {
         return nil, err
     }
 
-	return stats, nil
+    return stats, nil
 }
 
 func (bt *Logstashbeat) GetJvmStats(u url.URL) (*JvmStats, error) {
@@ -91,7 +91,7 @@ func (bt *Logstashbeat) GetJvmStats(u url.URL) (*JvmStats, error) {
         return nil, err
     }
 
-	return stats, nil
+    return stats, nil
 }
 
 func (bt *Logstashbeat) GetProcessStats(u url.URL) (*ProcessStats, error) {
@@ -116,5 +116,5 @@ func (bt *Logstashbeat) GetProcessStats(u url.URL) (*ProcessStats, error) {
         return nil, err
     }
 
-	return stats, nil
+    return stats, nil
 }
